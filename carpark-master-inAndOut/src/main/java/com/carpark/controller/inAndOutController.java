83325647bf53475f08f5e59d6e3c8910e6ea35f4
xiaoheaixuexi <374800141@qq.com>
2024-03-09 15:50:51 +0800

    分模块3

---
+++ carpark-master-inAndOut/src/main/java/com/carpark/controller/inAndOutController.java
@@ -0,0 +1,12 @@
+package com.carpark.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class inAndOutController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello hjh";
+    }
+}
